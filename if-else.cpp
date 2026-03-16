cf5ddda40ef313bb1376a06f15189d90084d609f
Atharv Joshi <joshiatharva000@gmail.com>
2026-03-16 23:05:36 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+
+    cout<<"enter your marks: ";
+    cin>>marks;
+
+    if(marks>=40)
+    {
+        cout<<"you are passed";
+
+    }
+    else
+    {
+        cout<<"you are failed";
+    }
+    return 0;
+
+}
