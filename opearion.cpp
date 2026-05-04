46d2cc1ef89bf132f9f9fd18ed1cbb52ccd27f75
Atharv Joshi <joshiatharva000@gmail.com>
2026-05-04 20:04:41 +0530

    Add files via upload

---
+++ opearion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter Two Numbers:"<<endl;
+    cin>>a>>b;
+
+    cout<<"Addition:"<<a+b<<endl;
+    cout<<"Subtraction:"<<a-b<<endl;
+    cout<<"Multiplication:"<<a*b<<endl;
+    cout<<"Division:"<<a/b<<endl;
+    cout<<"Modulus:"<<a%b<<endl;
+
+    return 0;
+}
+
